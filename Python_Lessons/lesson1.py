526b9c2529709fc9a965c2741b72eddf57e37f77
Precious Nobonga-Nkomo <precious.nobonga@gmail.com>
2026-07-15 23:28:36 +0200

    Create lesson1.py

---
+++ Python_Lessons/lesson1.py
@@ -0,0 +1,15 @@
+# Making the computer say hello
+# print("Hello World!")
+
+# storing data inside a variable
+#name = "Potso"
+#print(name)
+
+# printing name as a string
+#print("name")
+
+# Making our code more interactive
+name = input("Enter your name: ")
+print(f"Greetings, {name}")
+# f is called an f-string (or formatted string literal). It lets you insert
+#the vale of variables or expressions directly into a string using curly braces
