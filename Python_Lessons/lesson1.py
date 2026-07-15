076a76290c41fdcbbd672328ff78c95624ac1b2b
Precious Nobonga-Nkomo <precious.nobonga@gmail.com>
2026-07-16 01:50:31 +0200

    Update lesson1.py

--- Python_Lessons/lesson1.py
+++ Python_Lessons/lesson1.py
@@ -5,11 +5,37 @@
 #name = "Potso"
 #print(name)
 
+#age = 20
+#price = 19.95
+#first_name = "Potso"
+#is_online = False
+#print(age)
+#print(price)
+
+
 # printing name as a string
 #print("name")
 
 # Making our code more interactive
-name = input("Enter your name: ")
-print(f"Greetings, {name}")
+#name = input("Enter your name: ")
+#print(f"Greetings, {name}")
 # f is called an f-string (or formatted string literal). It lets you insert
 #the vale of variables or expressions directly into a string using curly braces
+
+# receiving input from the user
+#name = input("What is your name? ")
+#print("Hello, " + name)
+
+# birth_year =input("Enter your birth year: ")
+# this input function will return a value that will be stored in the variable birth_year
+#age = 2026 - int(birth_year)
+# the int function converts the string value of birth_year to an integer so that we can perform arithmetic operations on it
+# print(age)
+
+# int(); float(); bool(); and str() are built-in functions that convert the data type of a value
+
+# calculating the sum of two numbers
+value1 = input("Enter value1: ")
+value2 = input("Enter value2: ")
+sum = int(value1) + int(value2)
+print("The sum of the two values is " + str(sum))
